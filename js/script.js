94758520b4ec1e0b314d96324b57bc132247c9b3
Jarvis Tovar <jarvis2@gmail.com>
2024-08-18 17:21:30 -0400

    Se agregan nuevas validaciones

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const mensaje = document.querySelector(".mensaje");
 const botonCopiar = document.querySelector(".copiar");
 
 /*Las "llaves" de encriptación que utilizaremos son las siguientes:
-
 La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
 La letra "a" es convertida para "ai"
@@ -14,6 +13,10 @@ const restringe = /^[a-z\s]+$/;  //Para restringir minusculas sin acentos
 
 function encryptBtn(){
     if (validarEntrada(textArea.value)) {
+        if (yaEstaEncriptado(textArea.value)) {
+            alert("El texto ya está encriptado.");
+            return;
+        }
         const textoEncriptado = encriptar(textArea.value);
         mensaje.value = textoEncriptado;
         textArea.value = "";
@@ -22,9 +25,48 @@ function encryptBtn(){
     }
 }
 
+function decryptBtn(){
+    if (validarEntrada(textArea.value)) {
+        if (!sinEncriptar(textArea.value)) {
+            alert("El texto no está encriptado.");
+            return;
+        }
+        const textoEncriptado = desEncriptar(textArea.value);
+        mensaje.value = textoEncriptado;
+        textArea.value = "";
+        mensaje.style.backgroundImage = "none";
+        botonCopiar.style.display = "initial";
+    }
+}
+
+function yaEstaEncriptado(text) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    text = text.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (text.includes(matrizCodigo[i][1])) {
+            return true; // Ya está encriptado
+        }
+    }
+
+    return false; // No está encriptado
+}
+
+function sinEncriptar(text) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    text = text.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (text.includes(matrizCodigo[i][1])) {
+            return true; // Contiene al menos una parte encriptada
+        }
+    }
+
+    return false; // No contiene ninguna parte encriptada
+}
+
 function encriptar(stringEncriptado){
     let matrizCodigo = [["e" ,"enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    //console.log(matrizCodigo);
     stringEncriptado = stringEncriptado.toLowerCase();
 
     for(let i = 0; i < matrizCodigo.length; i++){
@@ -33,23 +75,10 @@ function encriptar(stringEncriptado){
         }
     }
     return stringEncriptado;
-
-}
-
-function decryptBtn(){
-    if (validarEntrada(textArea.value)) {
-        const textoEncriptado = desEncriptar(textArea.value);
-        mensaje.value = textoEncriptado;
-        textArea.value = "";
-        mensaje.style.backgroundImage = "none";
-        botonCopiar.style.display = "initial";
-
-    }
 }
 
 function desEncriptar(stringDesEncriptado){
     let matrizCodigo = [["e" ,"enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    //console.log(matrizCodigo);
     stringDesEncriptado = stringDesEncriptado.toLowerCase();
 
     for(let i = 0; i < matrizCodigo.length; i++){
@@ -58,7 +87,6 @@ function desEncriptar(stringDesEncriptado){
         }
     }
     return stringDesEncriptado;
-
 }
 
 function copiarTexto(){
